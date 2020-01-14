4b77b6f33573c59189123883c1ea6d54447c4a51
Diego Canales <diego@clearmetal.com>
2020-01-14 13:23:39 -0800

    Create gauge to track latency

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import os
 import random
 import time
 from flask import Flask, Response
-from prometheus_client import Counter, generate_latest
+from prometheus_client import Counter, Gauge, generate_latest
 
 
-my_basic_counter = Counter(
-    'my_basic_counter',
-    'A basic counter.'
-)
+request_latency_gauge = Gauge('request_latency_seconds', 'Measure request latency')
+
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
 MAX_SLEEP = float(os.environ.get('MAX_SLEEP', 1.5))
 
@@ -18,13 +16,14 @@ app = Flask(__name__)
 def hello():
     sleep_time = random.uniform(0, MAX_SLEEP)
     time.sleep(sleep_time)
+    num_hello_requests.inc()
+    request_latency_gauge.set(sleep_time)
 
     return "Slept for " + str(sleep_time)
 
 @app.route('/metrics', methods=['GET'])
 def get_data():
     """Returns all data as plaintext."""
-    my_basic_counter.inc()
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
